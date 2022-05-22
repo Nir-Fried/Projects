81d13b9774711bb35abdef1f088988830a40ae01
NirreF <103856974+NirreF@users.noreply.github.com>
2022-05-22 04:25:14 +0300

    Add files via upload

---
+++ hw1-softwareProject/kmeans.c
@@ -0,0 +1,330 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <math.h>
+
+int K = 0;
+int N = 0;
+int max_iter =0;
+char *input;
+char *output;
+int base = 10;
+int i=0;
+int j =0;
+int i1=0;
+int j1=0;
+int z1=0;
+int z=0;
+int x =0;
+int len=0;
+char *ptr;
+char *eptr;
+FILE *inputFile = NULL;
+FILE *outputFile = NULL;
+int d=1;
+char line[256];
+double **clusters = 0;
+double* centroids = 0;
+double* centroidsHolder = 0;
+double* dataPoints = 0;
+char*token = NULL;
+int iter =0;
+double epsilon = 0.001;
+double sum=0;
+double norm=0;
+double min=0;
+int minIndex=0;
+int temp =0;
+int* numInCluster = 0;
+int count = 0;
+int a =0;
+int b =0;
+int c =0;
+int rip =0;
+int flag = 1;
+char final[256];
+int counter1=0;
+
+int is_number(char *str, int *base)
+{
+    if (str == NULL) {
+        return 0; }
+
+    len = strlen(str);
+
+    if (len == 1)
+    {
+        *base = 10;
+        return isdigit(str[0]);
+    }
+
+    if ((str[0] == '0') && (str[1] == 'x'))
+    {
+        for (i = 2; i < len; i++)
+        {
+            char c = str[i];
+            c = tolower(c);
+            if (!(
+                (c >= '0' && c <= '9') || 
+                (c >= 'a' && c <= 'f'))) {
+                return 0; }
+        }
+        *base = 16;
+    }
+    else
+    {
+        i = 0;
+        if (str[0] == '-' || str[0] == '+') {
+            i = 1; }
+
+        for (; i < len; i++)
+        {
+            if (!isdigit(str[i])) {
+                return 0; }
+        }
+        *base = 10;
+    }
+    return 1;
+}
+
+int main(int argc,char * argv[])
+{
+    if(is_number(argv[1],&base)==0) /* if k isn't an integer */
+    {
+        printf("Invalid Input!");
+        return 1;
+    } 
+    if (argc != 4 && argc != 5) /* if number of inputs is invalid */
+    {
+        printf("Invalid Input!");
+        return 1;
+    }
+    
+    if (argc == 4) /* first case, without max_iter */
+    {
+        K = strtol(argv[1],&ptr,10);
+        input = malloc( sizeof(char) * (strlen(argv[2])+1) );
+        output = malloc( sizeof(char) * (strlen(argv[3])+1) );
+        strcpy(input,argv[2]);
+        strcpy(output,argv[3]);
+        max_iter = 200; /* if not provided the default value is 200 */
+    }
+    else if(argc == 5) /* second case, with max_iter */
+    {
+        if(is_number(argv[2],&base)==0) /* checking if max_iter is an integer */
+        {
+            printf("Invalid Input!");
+            return 1;
+        } 
+        K = strtol(argv[1],&ptr,10);
+        max_iter= strtol(argv[2],&ptr,10);
+        input = malloc( sizeof(char) * (strlen(argv[3])+1) );
+        output = malloc( sizeof(char) * (strlen(argv[4])+1) );
+        strcpy(input,argv[3]);
+        strcpy(output,argv[4]);
+    }
+    if (max_iter<1)
+    {
+        printf("Invalid Input!");
+        return 1;
+    }
+
+
+    inputFile = fopen(input,"r");
+
+    if (inputFile == NULL) /* if file was not found */
+    {
+        printf("An Error Has Occurred");
+        return 1;
+    }
+    
+    fgets (line , 256 , inputFile);
+    len = strlen(line);
+    for (i=0;i<len;i++)
+    {
+        d += (line[i] == ','); /* calculating our d value */
+    }   
+    fclose(inputFile);
+
+    inputFile = fopen(input,"r");
+    
+    while(fgets(line,256,inputFile)) /* calculating our N value */
+    {
+        line[strcspn(line,"\n")] =0; 
+        N++;
+    }
+    fclose(inputFile);
+
+
+    /* checking if K's input is valid:(1<K<N) */
+    if (K<1 || K>=N)
+    {
+        printf("Invalid Input!\n");
+        return 1;
+    }
+
+    dataPoints = realloc(dataPoints,d*N*sizeof(double));
+    numInCluster= realloc(numInCluster,K*sizeof(int));
+    centroids = realloc(centroids,d*K * sizeof(double));
+    centroidsHolder=realloc(centroidsHolder,d*K*sizeof(double));
+    for(i=0;i<K;i++)
+    {
+        clusters = (double **) realloc(clusters,K*sizeof(*clusters));
+        clusters[i] = (double *)malloc(N*d*sizeof(double));
+    }
+
+    for (i=0;i<K;i++) /* init numInCluster */
+    {
+        numInCluster[i] = 0;
+    }
+    for (i=0;i<K;i++) /* init clusters */
+    {
+        for(j=0;j<N*d;j++)
+        {
+            clusters[i][j]=0;
+        }
+    }
+
+
+    inputFile = fopen(input,"r");
+    i=0;
+    while(fgets(line,256,inputFile))
+    {
+        token = strtok(line,",");
+        while (token != NULL) {
+            dataPoints[i] = strtod(token,&eptr); /* initializing dataPoints */
+            if (i<K*d) 
+            {
+                centroids[i] = dataPoints[i]; /* initializing centroids. */
+            }
+            i++;
+            token = strtok(NULL, ",");
+        }
+    }
+    fclose(inputFile);
+
+    /* repeat: */
+    while(flag==1 && iter<max_iter)
+    {
+        for (a=0;a<K;a++)
+        {
+            for(b=0;b<N*d;b++)
+            {
+                clusters[a][b]=0;
+            }
+        }
+        for (x=0;x<K;x++) /*reset num in each cluster*/
+        {
+            numInCluster[x]=0;
+        }
+        for (i1=0; i1<N*d; i1=i1+d)
+        {
+            min = 10000000;
+            for(j1=0;j1<K*d;j1=j1+d)
+            {
+                sum =0;
+                for (z1=0;z1<d;z1++)
+                {
+                    sum = sum + pow(dataPoints[i1+z1]-centroids[j1+z1],2); /* (x_i-mu_j)^2*/
+                }
+                if (sum<min)
+                {
+                    min = sum;
+                    minIndex = j1/d; /* d =/= 0 */
+                }
+            }
+            for (z1=0;z1<d;z1++)
+            {
+                temp = numInCluster[minIndex];
+                clusters[minIndex][temp] = dataPoints[i1+z1];
+                numInCluster[minIndex]+=1;
+            }
+        }
+        /* update the centroids */
+        /* first we copy the current centroids to another array in order to check if new-old<epsilon) */
+        for (c=0;c<K*d;c++)
+        {
+            centroidsHolder[c]=centroids[c];
+        }
+        /* calculate the new centroids*/
+        rip=0;
+        for (i1=0;i1<K;i1++)
+        {   
+            count=0;
+            while(count<d) {
+            sum=0;
+            for(j1=count;j1<N*d;j1=j1+d)
+            { 
+                sum = sum + clusters[i1][j1];
+            }
+            if(numInCluster[i1] != 0)
+            {
+                centroids[rip+count] = sum/(numInCluster[i1]/d);
+                count++;
+            }
+            else if(numInCluster[i1] == 0) /* if clsuter is empty (it was later said in the forum that it won't happen)*/
+            {
+                centroids[rip+count] = 0;
+                count++;
+            }
+            }
+            rip = rip +d;
+        }
+
+        /* now we want to check if ||new-old||<epsilon for every vectors in centroids[] */
+        for(c=0;c<K*d;c++)
+        {
+            centroidsHolder[c]= fabs(centroidsHolder[c])-fabs(centroids[c]);
+        }
+        
+
+        counter1=0;
+        for(c=0;c<K*d;c=c+d)
+        {
+            sum=0;
+            for(z=0;z<d;z++)
+            {
+                sum = sum + pow(centroidsHolder[c+z],2);
+            }
+            norm = sqrt(sum);
+            if (norm < epsilon) {
+                counter1++;
+                if (counter1==K)
+                {
+                    flag=0;
+                }
+            }
+        }
+
+        iter++;
+    } 
+
+    outputFile = fopen(output,"w"); /* insert the centroids to the output file */
+
+    for(i=0;i<K*d;i=i+d)
+    {
+        for(j=0;j<d;j++)
+        {
+            fprintf(outputFile,"%0.4f",centroids[i+j]);
+            if(j != d-1)
+            {
+                fputs(",",outputFile);
+            }
+        }
+        fputc('\n',outputFile);
+    }
+    fclose(outputFile);
+
+
+    free(numInCluster);
+    free(dataPoints);
+    for (i = 0; i < K; i++)
+        free(clusters[i]);
+    free(clusters);
+    free(centroids);
+    free(centroidsHolder);
+    free(input);
+    free(output); 
+    return 0;
+}
---
+++ hw1-softwareProject/kmeans.py
@@ -0,0 +1,163 @@
+import sys
+import math
+
+if len(sys.argv) != 4 and len(sys.argv) != 5:
+    print('Invalid input!')
+    quit()
+
+if len(sys.argv) == 4: #without max_iter
+    K = sys.argv[1]
+    max_iter = "200" #if not provided, max_iter = 200
+    input = sys.argv[2]
+    output = sys.argv[3]
+elif len(sys.argv)==5: #with max_iter
+    K = sys.argv[1]
+    max_iter = sys.argv[2]
+    input = sys.argv[3]
+    output = sys.argv[4]
+
+#Calculate d and N.
+
+d=1
+try:
+    file = open(input,'r')
+    line = file.readline()
+    for chars in line:
+        if chars==',':
+            d = d+1
+    file.close()
+except FileNotFoundError:
+    print('An Error Has Occurred')
+    quit()
+
+N=0
+file = open(input,'r')
+line = file.readline()
+while line !='':
+    line=file.readline()
+    N=N+1
+file.close()
+
+#check validation of inputs.
+
+if K.isdigit()==False: #checking if K in an integer
+    print('Invalid input!')
+    quit()
+
+if max_iter.isdigit()==False: #checking if max_iter is an integer
+    print('Invalid Input!')
+    quit()
+
+#   Converting K and max_iter to ints:
+K = int(K)
+max_iter = int(max_iter)
+
+if K<2 or K>=N or max_iter<1:
+    print('Invalid Input!')
+    quit()
+
+dataPoints = [[0 for x in range(d)] for y in range(N)]
+centroids = [[0 for x in range(d)] for y in range(K)]
+centroidsHolder = [[0 for x in range(d)] for y in range(K)]
+numInCluster = [0 for x in range(K)]
+clusters = [[[0 for x in range(d)] for y in range(N)] for z in range(K)]
+
+file = open(input,'r') #init dataPoints
+line = file.readline()
+x=0
+while line !='':
+    lineList = line.split(",")
+    lineList[len(lineList)-1] = lineList[len(lineList)-1].rstrip("\n")
+    for y in range(d):
+        dataPoints[x][y] = float(lineList[y])
+    x = x+1
+    line=file.readline()   
+file.close()
+
+for x in range(K): #init centroids
+    for y in range(d):
+        centroids[x][y] = dataPoints[x][y]
+
+
+flag = True
+iter = 0
+
+while flag==True and iter<max_iter: #repeat:
+
+    for x in range(K):
+        for y in range(N):
+            for z in range(d):
+                clusters[x][y][z] = 0
+
+    for x in range(K):
+        numInCluster[x] = 0
+
+
+    for x in range(N):
+        min = 10000000000
+        for y in range(K):
+            sum = 0
+            for z in range(d):
+                sum = sum + ((dataPoints[x][z]-centroids[y][z])**2)   
+            if sum<min:
+                min = sum
+                minIndex = y
+        
+        for z in range(d):
+            temp = numInCluster[minIndex]
+            clusters[minIndex][x][z] = dataPoints[x][z]
+            numInCluster[minIndex] = numInCluster[minIndex]+1
+
+    # update the centroids
+    # first we copy the current centroids to another array in order to check if new-old<epsilon) 
+    for x in range(K):
+        for y in range(d):
+            centroidsHolder[x][y] = centroids[x][y]
+
+    #calculate new centroids
+    for x in range(K):
+        count = 0
+        while count<d:
+            sum = 0
+            for y in range(N):
+                sum = sum + clusters[x][y][count]
+            
+            if numInCluster[x] != 0:
+                centroids[x][count] = sum/(numInCluster[x]/d)
+                count = count+1
+            elif numInCluster[x] == 0: #if cluster is empty (it was later said in the forum that it won't happen)
+                centroids[x][count]=0
+                count = count +1
+    
+    # now we want to check if ||new-old||<epsilon for every vector in centroids[][]
+    for x in range(K):
+        for y in range(d):
+            centroidsHolder[x][y] = abs(centroidsHolder[x][y]) - abs(centroids[x][y])
+    
+    counter1=0
+    for x in range(K):
+        sum =0
+        for y in range(d):
+            sum = sum + (centroidsHolder[x][y]**2)
+        norm = math.sqrt(sum)
+        if norm < 0.001:
+            counter1 = counter1 + 1
+            if counter1==K:
+                flag = False
+    
+    iter = iter+1
+
+
+file = open(output,'w')
+for x in range(K):
+    line = ""
+    for y in range(d):
+        centroids[x][y] = format(centroids[x][y],'.4f')
+        line = line + str(centroids[x][y])
+        if y != d-1:
+            line = line + ","
+    line = line + "\n"
+    file.write(line)
+file.close()
+
+
